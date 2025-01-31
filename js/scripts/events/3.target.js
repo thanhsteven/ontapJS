bea3793ae163ee0253a613ba1659e0516c79c8f8
thanhsteven <kimthanhpham96@gmail.com>
2025-01-31 13:44:10 +0700

    File Changing Color

--- js/scripts/events/3.target.js
+++ js/scripts/events/3.target.js
@@ -3,12 +3,12 @@
 
 const btn2 = document.querySelector(".btn-2");
 btn2.addEventListener("click", function (event) {
-  console.log(event.target); // Chọn chính xác cái element mà mình click tới
-  console.log(event.currentTarget); // Chọn cái phần tử mà mình click
+  console.log(event.target); // Chọn chính xác cái element (thẻ tag html) mà mình click tới
+  console.log(event.currentTarget); // Chọn cái phần tử (thẻ tag html bao bọc bên ngoài) mà mình click
 });
 
 // * event.preventDefault()
-// Ngăn chặn một hành vi mặc định nào đó
+// Ngăn chặn một hành vi mặc định nào đó của thẻ tag html
 const btnLink = document.querySelector(".btn-link");
 btnLink.addEventListener("click", function (e) {
   e.preventDefault(); // Ngăn chặn chuyển hướng của href
